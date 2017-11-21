da1774896d188e325c708eb3d26b89224098aaba
lintangsutawika <lintangsutawika@gmail.com>
2017-11-21 11:03:43 +0700

    Problem 1 done, working on problem 2

---
+++ toyRF.py
@@ -0,0 +1,85 @@
+from random import *
+
+class EasyEnv:
+    def __init__(self):
+        self.prev_obs = 0
+        self.resetState = 0
+        self.resetFlag = 0
+        self.stepNum = 0
+        self.new_obs = 0
+
+    def reset(self):
+        self.resetState = randint(0,1)*2-1
+        self.resetFlag = 1
+        self.stepNum = 0
+        return self.resetState
+
+    def obs_dim(self):
+        return 1
+
+    def step(self, action):
+        if self.stepNum >= 10:
+            stopSignal = 1
+            self.stepNum = 0
+        else:
+            stopSignal = 0
+            self.stepNum += 1
+
+        if self.resetFlag == 1:
+            prev_obs = self.resetState
+            self.resetFlag = 0
+        else:
+            prev_obs = self.new_obs  
+
+        self.new_obs = randint(0,1)*2-1
+        reward = action * prev_obs
+        return [self.new_obs, stopSignal, reward]
+
+    def test(self):
+        pass
+
+class CartEnv(object):
+    def __init__(self):
+        self.prev_obs = 0
+        self.resetState = 0
+        self.resetFlag = 0
+        self.stepNum = 0
+        self.new_obs = 0
+
+    def reset(self, states):
+        print('r obs {} {} {} {}'.format())
+        sys.stdout.flush()
+        self.resetState = randint(0,1)*2-1
+        self.resetFlag = 1
+        self.stepNum = 0
+        return self.resetState
+
+    def obs_dim(self):
+        return 1
+
+    def step(self, action):
+        print('s {}'.format(action))
+        if self.stepNum >= 500:
+            stopSignal = 1
+            self.stepNum = 0
+        else:
+            stopSignal = 0
+            self.stepNum += 1
+
+        if self.resetFlag == 1:
+            prev_obs = self.resetState
+            self.resetFlag = 0
+        else:
+            prev_obs = self.new_obs  
+
+        self.new_obs = randint(0,1)*2-1
+        reward = 1
+        return [self.new_obs, stopSignal, reward]
+
+    def test(self):
+        pass
+class LinearModel(object):
+    """docstring for LinearModel"""
+    def __init__(self):
+        pass
+        
